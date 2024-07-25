716d34c650c9e1419c355c48e0e547787db7fc65
Oyundari23 <dariaa0560@gmail.com>
2024-07-25 17:56:17 +0800

    7/25 angid

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,37 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [articles, setArticles] = useState([]);
+
+
+  useEffect(() => {
+    fetch("https://dev.to/api/articles?username=zacharylee")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setArticles(data);
+      });
+
+  }, []);
+
+
   return (
-    useEffect(() => {
-      fetch("https://dev.to/api/articles?username=ben").then((response) => { return response.json() }).then((data) => console.log('data', data))
-    }, [])
-   
-   
- 
+    <div className="container mx-auto">
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {articles.map((item) => (
+          <div key={item.id} className="shadow-lg card bg-base-100">
+            <div className="card-body">
+              <Image src={item.social_image} width={500} height={500}></Image>
+              <Link href={item.url} target="_blank">
+                {item.title}
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
